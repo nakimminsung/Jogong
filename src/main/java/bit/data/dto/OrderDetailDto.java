a87cfa0a3e05b87597cf1d8350f1f637e5224007
kkookkss <pre0919@gmail.com>
2022-09-26 15:49:02 +0900

    dto setting

---
+++ src/main/java/bit/data/dto/OrderDetailDto.java
@@ -0,0 +1,13 @@
+package bit.data.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderDetailDto {
+	private int num;
+	private int qty;
+	private int friendNum;
+	private int userNum;
+	private int productNum;
+	private int orderNum;
+}
