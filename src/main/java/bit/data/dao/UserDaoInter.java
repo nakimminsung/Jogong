c98d2923e211d56515ce6b73b1123ad8351185eb
kkookkss <pre0919@gmail.com>
2022-10-13 18:44:22 +0900

    Feat: friend

--- src/main/java/bit/data/dao/UserDaoInter.java
+++ src/main/java/bit/data/dao/UserDaoInter.java
@@ -16,10 +16,6 @@ public interface UserDaoInter {
 	// 리뷰페이지
 	public List<UserDto> getReviewUserInfo();
 	public List<UserDto> selectNonFriendlyUsersList(Map<String,Object> map);
-	
-	/*
-	 * //카카오 public void insertKakao(UserDto dto);
-	 */
 	//회원정보수정
 	public void updateUser(UserDto dto);
 	// 마이페이지 문의내역
